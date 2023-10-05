dae9e01fdb50083bb7d8e1488fe248950166a4df
heahgo <h34hg0@gmail.com>
2023-10-05 12:41:53 +0900

    Include cstdio and Fix int->uint8_t

--- include/aes/aes.h
+++ include/aes/aes.h
@@ -1,22 +1,24 @@
 #pragma once
+#include <iostream>
+#include <cstdint>
 
 class AesCore {
     private:
-        int* ex_key_;
-        int* KeyExpantion(int* key, int size);
-        void AddRoundKey(int block[16], int key[16]);
-        void ShiftRows(int block[16]);
-        void InvShirfRows(int block[16]);
-        void MixColumns(int block[16]);
-        void InvMixColumns(int block[16]);
-        void SubBytes(int block[16]);
-        void InvSubBytes(int block[16]);
-        void EncBlock(int block[16]);
-        void DecBlock(int block[16]);
+        uint8_t* ex_key_;
+        uint8_t* KeyExpantion(uint8_t* key, uint8_t key_byte_size);
+        void AddRoundKey(uint8_t block[16], uint8_t key[16]);
+        void ShiftRows(uint8_t block[16]);
+        void InvShiftRows(uint8_t block[16]);
+        void MixColumns(uint8_t block[16]);
+        void InvMixColumns(uint8_t block[16]);
+        void SubBytes(uint8_t block[16]);
+        void InvSubBytes(uint8_t block[16]);
+        void EncBlock(uint8_t block[16]);
+        void DecBlock(uint8_t block[16]);
 
     public:
-        AesCore(int* key, int size) {
-            ex_key_ = KeyExpantion(key, size);
+        AesCore(uint8_t* key, uint8_t key_byte_size) {
+            ex_key_ = KeyExpantion(key, key_byte_size);
         }
         ~AesCore() {
             delete[] ex_key_;
