3e2dcb24b7bcdcf8badf96cc76f4b10f0a833fa7
heahgo <h34hg0@gmail.com>
2023-10-10 11:14:11 +0900

    Update crpyto/aes/aes_core.cpp

--- crpyto/aes/aes_core.cpp
+++ crpyto/aes/aes_core.cpp
@@ -11,16 +11,27 @@ uint8_t mult8(uint8_t x, uint8_t y) {
     return result;
 }
 
-uint8_t* AesCore::KeyExpantion(uint8_t* key, uint8_t key_byte_size) {
+AesCore::AesCore(uint8_t* key, uint8_t key_byte_size) {
+    uint8_t cols;
     if (key_byte_size == 16) {
-
+        cols = 10;
     } else if (key_byte_size == 24) {
-
+        cols = 12;
     } else if (key_byte_size == 32) {
-
+        cols = 14;
     } else {
         throw std::length_error("Key Length is only used 128, 192, 256 bit!!");
     }
+
+    ex_key_ = KeyExpantion(key, key_byte_size, cols);
+}
+
+AesCore::~AesCore() {
+    delete[] ex_key_;
+}
+
+uint8_t* AesCore::KeyExpantion(uint8_t* key, uint8_t key_byte_size, uint8_t cols) {
+
     // int* ex_key = new int[]
     // return ex_key
 }
