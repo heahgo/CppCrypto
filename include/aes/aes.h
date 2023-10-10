e2fb1bb946c8fa998c225eb0ce2cd0b74b686dd3
heahgo <h34hg0@gmail.com>
2023-10-10 11:13:51 +0900

    Update include/aes/aes.h

--- include/aes/aes.h
+++ include/aes/aes.h
@@ -3,12 +3,9 @@
 #include <iostream>
 
 class AesCore {
-    public:
-        uint8_t Mult8(uint8_t x, uint8_t y);
     private:
         uint8_t* ex_key_;
-        uint8_t* KeyExpantion(uint8_t* key, uint8_t key_byte_size);
-
+        uint8_t* KeyExpantion(uint8_t* key, uint8_t key_byte_size, uint8_t cols);
         void AddRoundKey(uint8_t block[16], uint8_t key[16]);
         void ShiftRows(uint8_t block[16]);
         void InvShiftRows(uint8_t block[16]);
@@ -18,14 +15,10 @@ class AesCore {
         void InvSubBytes(uint8_t block[16]);
         void EncBlock(uint8_t block[16]);
         void DecBlock(uint8_t block[16]);
-
     public:
-        AesCore(uint8_t* key, uint8_t key_byte_size) {
-            ex_key_ = KeyExpantion(key, key_byte_size);
-        }
-        ~AesCore() {
-            delete[] ex_key_;
-        }
+        uint8_t Mult8(uint8_t x, uint8_t y);
+        AesCore(uint8_t* key, uint8_t key_byte_size);
+        ~AesCore();
 };
 
 
