623d7fc6554d66fa7a1ba23ce514184fc5231c27
heahgo <h34hg0@gmail.com>
2023-10-28 05:25:40 +0900

    Rename funtion

--- include/aes/aes_ecb.h
+++ include/aes/aes_ecb.h
@@ -1,8 +1,8 @@
 #pragma once
-#include "../aes.h"
+#include "../aes/aes.h"
 
-class Aes_ecb(AesCore) {
+class Aes_ecb : public AesCore {
     public:
-        uint8_t* Encryption(uint8_t plain, uint32_t size);
-        uint8_t* Decryption(uint8_t cipher, uint32_t size);
+        uint8_t* Encrypt(uint8_t plain, uint32_t size);
+        uint8_t* Decrypt(uint8_t cipher, uint32_t size);
 }
