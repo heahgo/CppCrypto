8ab7e553b77795bdcd7b635a4d1ff1c76bc3edb1
heahgo <h34hg0@gmail.com>
2023-10-28 04:59:00 +0900

    Update include/aes/aes_ecb.h

--- include/aes/aes_ecb.h
+++ include/aes/aes_ecb.h
@@ -3,6 +3,6 @@
 
 class Aes_ecb(AesCore) {
     public:
-        uint8_t* Encryption(uint8_t text, uint32_t size);
-        uint8_t* Decryption(uint8_t text, uint32_t size);
+        uint8_t* Encryption(uint8_t plain, uint32_t size);
+        uint8_t* Decryption(uint8_t cipher, uint32_t size);
 }
