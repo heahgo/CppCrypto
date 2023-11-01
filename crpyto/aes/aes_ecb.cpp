e2e2ee545f0f7b0abae1369f59594bb6a7f3e226
heahgo <h34hg0@gmail.com>
2023-11-01 16:20:00 +0900

    Update codes

---
+++ crpyto/aes/aes_ecb.cpp
@@ -0,0 +1,16 @@
+#include "../../include/aes/aes_ecb.h"
+#include <cstdint>
+
+void Aes_ecb::Encrypt(uint8_t* plain, uint8_t* cipher, uint32_t size) {
+    if (size % 16 != 0)
+        throw std::out_of_range("Incorrect plaintext");
+    for (uint32_t i = 0; i < size; i+=16) {
+        memcpy(cipher+i, plain+i, 16);
+        aes_core_.EncBlock(cipher+i);
+    }
+}
+
+void Aes_ecb::Decrypt(uint8_t* cipher, uint8_t* plain, uint32_t size) {
+    if (size % 16 != 0)
+        throw std::out_of_range("Incorrect ciphertext");
+}
--- include/aes/aes_ecb.h
+++ include/aes/aes_ecb.h
@@ -6,6 +6,6 @@ class Aes_ecb : public Cipher {
         AesCore aes_core_;
         void Encrypt(uint8_t* plain, uint8_t* cipher, uint32_t size);
         void Decrypt(uint8_t* cipher, uint8_t* plain, uint32_t size);
-        Aes_ecb(uint8_t* key, uint8_t key_byte_size) : aes_core_(AesCore(key, key_byte_size)) {};
-        virtual ~Aes_ecb();
+        Aes_ecb(uint8_t* key, uint8_t key_byte_size) : aes_core_(AesCore(key, key_byte_size)) {}
+        virtual ~Aes_ecb() {}
 };
