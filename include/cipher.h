2237df43bf67285ed1c9adb6229b743d54be854f
heahgo <h34hg0@gmail.com>
2023-11-01 16:03:50 +0900

    Change function return type uint8_t* -> void

--- include/cipher.h
+++ include/cipher.h
@@ -2,6 +2,6 @@
 #include <cstdint>
 
 class Cipher {
-    virtual uint8_t* Encrypt(uint8_t* plain, uint8_t* cipher, uint32_t size) = 0;
-    virtual uint8_t* Decrypt(uint8_t* cipher, uint8_t* plain, uint32_t size) = 0;
+    virtual void Encrypt(uint8_t* plain, uint8_t* cipher, uint32_t size) = 0;
+    virtual void Decrypt(uint8_t* cipher, uint8_t* plain, uint32_t size) = 0;
 };
