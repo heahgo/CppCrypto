36590f72ca3edc3ab9d6e6647a2544b7ab566b78
heahgo <h34hg0@gmail.com>
2023-11-01 16:29:14 +0900

    Implement member function Encrypt, Decrypt in crypto/aes/aes_ecb.cpp

--- crpyto/aes/aes_ecb.cpp
+++ crpyto/aes/aes_ecb.cpp
@@ -13,4 +13,8 @@ void Aes_ecb::Encrypt(uint8_t* plain, uint8_t* cipher, uint32_t size) {
 void Aes_ecb::Decrypt(uint8_t* cipher, uint8_t* plain, uint32_t size) {
     if (size % 16 != 0)
         throw std::out_of_range("Incorrect ciphertext");
+    for (uint32_t i = 0; i < size; i+=16) {
+        memcpy(plain+i, cipher+i, 16);
+        aes_core_.DecBlock(plain+i);
+    }
 }
