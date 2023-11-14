f5eeb95d16a4e4f62361ac5533086101e984bb5c
heahgo <h34hg0@gmail.com>
2023-11-14 17:03:59 +0900

    Retype uint8_t* -> Bytes and Update codes

--- crpyto/aes/aes_ecb.cpp
+++ crpyto/aes/aes_ecb.cpp
@@ -1,20 +1,28 @@
 #include "../../include/aes/aes_ecb.h"
 #include <cstdint>
 
-void Aes_ecb::Encrypt(uint8_t* plain, uint8_t* cipher, uint32_t size) {
+Bytes Aes_ecb::Encrypt(Bytes& plain) {
+    uint8_t size = plain.size();
     if (size % 16 != 0)
         throw std::out_of_range("Encrypt Error : Incorrect plaintext length");
+    uint8_t* cipher = new uint8_t[size];
+    const uint8_t* p = plain.data();
     for (uint32_t i = 0; i < size; i+=16) {
-        memcpy(cipher+i, plain+i, 16);
+        memcpy(cipher+i, p+i, 16);
         aes_core_.EncBlock(cipher+i);
     }
+    return Bytes(cipher, size);
 }
 
-void Aes_ecb::Decrypt(uint8_t* cipher, uint8_t* plain, uint32_t size) {
+Bytes Aes_ecb::Decrypt(Bytes& cipher) {
+    uint8_t size = cipher.size();
     if (size % 16 != 0)
         throw std::out_of_range("Decrypt Error : Incorrect ciphertext lengrh");
+    uint8_t* plain = new uint8_t[size];
+    const uint8_t* c = cipher.data();
     for (uint32_t i = 0; i < size; i+=16) {
-        memcpy(plain+i, cipher+i, 16);
+        memcpy(plain+i, c+i, 16);
         aes_core_.DecBlock(plain+i);
     }
+    return Bytes(plain, size);
 }
--- include/aes/aes_ecb.h
+++ include/aes/aes_ecb.h
@@ -4,8 +4,8 @@
 class Aes_ecb : public Cipher {
     public:
         AesCore aes_core_;
-        void Encrypt(uint8_t* plain, uint8_t* cipher, uint32_t size);
-        void Decrypt(uint8_t* cipher, uint8_t* plain, uint32_t size);
+        Bytes Encrypt(Bytes& plain);
+        Bytes Decrypt(Bytes& cipher);
         Aes_ecb(uint8_t* key, uint8_t key_byte_size) : aes_core_(AesCore(key, key_byte_size)) {}
         virtual ~Aes_ecb() {}
 };
