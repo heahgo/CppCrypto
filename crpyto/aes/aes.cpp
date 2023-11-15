1d9d3322bc59aadfd21c84b04c3e66b81cef1800
heahgo <h34hg0@gmail.com>
2023-11-15 14:28:30 +0900

    Update Codes

--- crpyto/aes/aes.cpp
+++ crpyto/aes/aes.cpp
@@ -1,1 +1,17 @@
 #include "../../include/aes/aes.h"
+
+AES::AES(Bytes key, string mode, Bytes iv = Bytes()) {
+
+}
+
+AES::~AES() {
+    
+}
+
+Bytes AES::Encrypt(Bytes plain) {
+
+}
+
+Bytes AES::Decrypt(Bytes cipher) {
+
+}
--- include/aes/aes.h
+++ include/aes/aes.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "./aes_ecb.h"
+using std::string;
 
 class AES {
+    private:
+        string mode;
 
+    public:
+        Bytes Encrypt(Bytes plain);
+        Bytes Decrypt(Bytes cipher);
+        AES(Bytes key, string mode, Bytes iv = Bytes());
+        virtual ~AES();
 };
