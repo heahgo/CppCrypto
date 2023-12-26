473c6bf364d11d513c059f8924f9e8a1be9a2a00
heahgo <h34hg0@gmail.com>
2023-12-26 16:00:59 +0900

    Update aes_table.h aes_table.cpp

--- crypto/aes/aes_table.cpp
+++ crypto/aes/aes_table.cpp
@@ -1,14 +1,6 @@
 #include "../../include/aes/aes_table.h"
 
 AesTable::AesTable(Bytes& key) : AesCore(key) {
-     // Memory allocation for T-boxes
-    t_boxes = new uint8_t**[round_];
-    for (uint8_t r = 0; r < round_; r++) {
-        t_boxes[r] = new uint8_t*[block_size_];
-        for (uint8_t i = 0; i < block_size_; i++) {
-            t_boxes[r][i] = new uint8_t[256];
-        } 
-    }
     // T-boxes generation
     for (uint8_t r = 0; r < round_-1; r++) {   
         for (uint8_t i = 0; i < block_size_; i++) {
@@ -22,12 +14,6 @@ AesTable::AesTable(Bytes& key) : AesCore(key) {
             t_boxes[round_-1][i][j] = sbox[j ^ ex_key_[(round_-1)*block_size_+i]] ^ ex_key_[round_*block_size_+i];
         }
     }
-
-    // Memory allocation for Ty-tables
-    ty_tables = new uint32_t*[4];
-    for (uint8_t i = 0; i < 4; i++) {
-        ty_tables[i] = new uint32_t[256];
-    }
     // Ty-tables generation
     for (uint32_t i = 0; i < 256; i++) {
         ty_tables[0][i] = Mult8(i, 0x02)<<24 | i<<16 | i<<8 | Mult8(i, 0x03);
@@ -35,10 +21,15 @@ AesTable::AesTable(Bytes& key) : AesCore(key) {
         ty_tables[2][i] = i<<24 | Mult8(i, 0x03)<<16 | Mult8(i, 0x02)<<8 | i;
         ty_tables[3][i] = i<<24 | i<<16 | Mult8(i, 0x03)<<8 | Mult8(i, 0x02);
     }
-    // Memory allocation for XOR-tables
-    xor_tables = new uint8_t*[16];
-    for (uint8_t i = 0; i < 16; i++) {
-        xor_tables[i] = new uint8_t[16];
+    // composed T-boxes/Ty-tables generation
+    for (uint8_t i = 0; i < 4; i++) {
+        for (uint8_t r = 0; r < round_-1; r++) {
+            for (uint8_t j = 0; j < block_size_; j++) {
+                for (uint32_t k = 0; k < 256; k++) {
+                    t_boxes_ty_tables[i][r][j][k] = ty_tables[i][t_boxes[r][j][k]];
+                }
+            }
+        }
     }
     // XOR-tables generation
     for (uint8_t i = 0; i < 16; i++) {
@@ -48,20 +39,32 @@ AesTable::AesTable(Bytes& key) : AesCore(key) {
     }
 } 
 
-AesTable::~AesTable() {
-    // Memory deallocation for T-boxes
-    for(uint8_t r = 0; r < round_; r++) {
-        for (uint8_t i = 0; i < block_size_; i++) {
-            delete[] t_boxes[r][i];
+AesTable::~AesTable() {}
+
+void AesTable::EncBlock(uint8_t block[16]) {
+    uint32_t a, b, c, d;
+    for (uint8_t i = 0; i < round_-1; i++) {
+        ShiftRows(block);
+        for (uint8_t j = 0; j < 4; j++) {
+            //t_boxes_ty_tables
+            a = t_boxes_ty_tables[0][i][j*4+0][block[j*4+0]];
+            b = t_boxes_ty_tables[1][i][j*4+1][block[j*4+1]];
+            c = t_boxes_ty_tables[2][i][j*4+2][block[j*4+2]];
+            d = t_boxes_ty_tables[3][i][j*4+3][block[j*4+3]];
+            //xor_tables
+            block[j*4+0] = (xor_tables[xor_tables[(a>>0)&0x0f][(b>>0)&0x0f]][xor_tables[(c>>0)&0x0f][(d>>0)&0x0f]]) |
+                            (xor_tables[xor_tables[(a>>4)&0x0f][(b>>4)&0x0f]][xor_tables[(c>>4)&0x0f][(d>>4)&0x0f]]<<4);
+            block[j*4+1] = (xor_tables[xor_tables[(a>>8)&0x0f][(b>>8)&0x0f]][xor_tables[(c>>8)&0x0f][(d>>8)&0x0f]]) |
+                            (xor_tables[xor_tables[(a>>12)&0x0f][(b>>12)&0x0f]][xor_tables[(c>>12)&0x0f][(d>>12)&0x0f]]<<4);
+            block[j*4+2] = (xor_tables[xor_tables[(a>>16)&0x0f][(b>>16)&0x0f]][xor_tables[(c>>16)&0x0f][(d>>16)&0x0f]]) |
+                            (xor_tables[xor_tables[(a>>20)&0x0f][(b>>20)&0x0f]][xor_tables[(c>>20)&0x0f][(d>>20)&0x0f]]<<4);
+            block[j*4+3] = (xor_tables[xor_tables[(a>>24)&0x0f][(b>>24)&0x0f]][xor_tables[(c>>24)&0x0f][(d>>24)&0x0f]]) |
+                            (xor_tables[xor_tables[(a>>28)&0x0f][(b>>28)&0x0f]][xor_tables[(c>>28)&0x0f][(d>>28)&0x0f]]<<4);
         }
-        delete[] t_boxes[r]; 
     }
-    // Memory deallocation for Ty-tables
-    for (uint8_t i = 0; i < 4; i++) {
-        delete[] ty_tables[i];
-    }
-    // Memory deallocation for XOR-tables
-    for (uint8_t i = 0; i < 16; i++) {
-        delete[] xor_tables[i];
+    ShiftRows(block);
+    //t_boxes
+    for (uint8_t i = 0; i < block_size_; i++) {
+        block[i] = t_boxes[round_-1][i][block[i]];
     }
 }
--- include/aes/aes_table.h
+++ include/aes/aes_table.h
@@ -2,11 +2,12 @@
 
 class AesTable : public AesCore {
     private:
-        uint8_t*** t_boxes;
-        uint32_t ** ty_tables;
-        uint8_t **xor_tables;
+        uint8_t t_boxes[9][16][256];
+        uint32_t ty_tables[4][256];
+        uint32_t t_boxes_ty_tables[4][9][16][256];
+        uint8_t xor_tables[16][16];
     public:
-        void PrintTyTables();
+        void EncBlock(uint8_t block[16]);
         AesTable(Bytes& key);
         ~AesTable();
 };
