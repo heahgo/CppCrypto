a4d08d0692af92ce5c418150345ef6ecdf3f05dd
heahgo <h34hg0@gmail.com>
2023-12-29 11:07:09 +0900

    Update ecb_test

--- app/aes_test/ecb_test/aes_ecb.py
+++ app/aes_test/ecb_test/aes_ecb.py
@@ -6,11 +6,11 @@ key_size = [128, 192, 256]
 
 for i in range(3):
     with open(f"test-{key_size[i]}.txt", "w") as f:
+        key = get_random_bytes(key_size[i] // 8)
+        cipher = AES.new(key, AES.MODE_ECB)
+        f.write(key.hex() + "\n")
         for _ in range(10000):
-            key = get_random_bytes(key_size[i] // 8)
-            cipher = AES.new(key, AES.MODE_ECB)
             plaintext = get_random_bytes(get_random_bytes(1)[0])
             ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
-            f.write(key.hex() + "\n")
             f.write(plaintext.hex() + "\n")
             f.write(ciphertext.hex() + "\n")
