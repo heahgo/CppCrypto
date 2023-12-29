55bde3d8743c851a8cbb1da11f76532f9c3f2546
heahgo <h34hg0@gmail.com>
2023-12-29 11:06:46 +0900

    Remove description

--- app/aes_test.cpp
+++ app/aes_test.cpp
@@ -28,14 +28,14 @@ bool Compare(Bytes &a, Bytes &b) {
 void Test(ifstream &test) {
     Bytes k, p,c ;
     string key, plaintext, ciphertext;
+    getline(test, key);
+    k = Bytes(key);
+    AES aes = AES(k, ECB_MODE);
     for (uint32_t i = 0; i < 10000; i++) {
-        getline(test, key);
         getline(test, plaintext);
         getline(test, ciphertext);
-        k = Bytes(key);
         p = Bytes(plaintext);
         c = Bytes(ciphertext);
-        AES aes = AES(k, ECB_MODE);
         p = Pad(p, 16);
         p = aes.Encrypt(p);
         if (!Compare(p,  c)) {
@@ -59,26 +59,4 @@ int main() {
     test_128.close();
     test_192.close();
     test_256.close();
-
-    // uint8_t k[16] = {0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79, 0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20, 0x46, 0x75};
-    // uint8_t plain[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-    // uint8_t a[] = {0x12, 0x34, 0x56, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-
-    // Bytes key(k, 16);
-    // Bytes p(plain, 16);
-    // Aes_ecb crypto = Aes_ecb(key);
-    // PrintHex(p.data(), p.size());
-    // p = Pad(p, 16);
-    // p = crypto.Encrypt(p);
-    // PrintHex(p.data(), p.size());
-    // p = crypto.Decrypt(p);
-    // p = Unpad(p, 16);
-    // PrintHex(p.data(), p.size());
-    // AES aes = AES(key, ECB_MODE);
-    // p = Pad(p, 16);
-    // p = aes.Encrypt(p);
-    // PrintHex(p.data(), p.size());
-    // p = aes.Decrypt(p);
-    // p = Unpad(p, 16);
-    // PrintHex(p.data(), p.size());
 }  
